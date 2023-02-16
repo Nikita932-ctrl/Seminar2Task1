b97024fab4d42d401ddb2178e3db6e46b52d99c1
Nikita932-ctrl <65113631+Nikita932-ctrl@users.noreply.github.com>
2023-02-17 00:55:38 +0300

    Create task1.cs

---
+++ task1.cs
@@ -0,0 +1,39 @@
+﻿// нашите программу которая принимает на вход 3-х значное число и возвращает второй символ
+
+int FindSecondNumber(int number)
+{
+    int max = 999;
+    int min = 100;
+
+    if (number > min && number <= max)
+    {
+        number = number % 100;
+        number = number / 10;
+    }
+    else if (number >= max)
+    {
+        while(number > max)
+        {          
+            number--;           
+        }
+        number = number % 100;
+        number = number / 10;
+    }else if (number < min | number == 0)
+    {
+        while (number <= min | number == 0)
+        {
+            number++;
+        }
+        number = number % 100;
+        number = number / 10;
+    }
+    return number;
+
+
+
+}
+
+Console.WriteLine(FindSecondNumber(999999));
+
+
+
